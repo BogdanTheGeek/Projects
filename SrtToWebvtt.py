5bfede91b31462a27fa98cc69c242b226bc2c19f
BogdanTheGeek <bobby.b.tiger@gmail.com>
2017-05-26 16:22:49 +0100

    Update SrtToWebvtt.py

--- SrtToWebvtt.py
+++ SrtToWebvtt.py
@@ -1,3 +1,6 @@
+#this is a short python script that converts .srt files into .vtt files for use in embedded videos. 
+#Just run the script with the name of the file as the argument and you will get a webvtt subtitle.
+
 import sys
 
 argument = str(sys.argv[1]) 
